68c3562cafcad94419b31aaa6026017c3a8af74a
Doris8462 <liandou123>
2020-09-17 19:17:10 +0800

    [huangunjie] feat:finish practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -9,8 +11,9 @@ import java.time.LocalDate;
  * @create 2020-05-15_16:33
  */
 public class Practice1 {
-
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        int nextYear=date.getMonthValue()>5?date.getYear() + 1:date.getYear();
+        LocalDate nextLaborDay = LocalDate.of(nextYear, 5, 1);
+        return nextLaborDay.toEpochDay()-date.toEpochDay();
+    }
 }
